e6163ee1fc9fb72c59923607acfb88c314609a09
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2024-02-28 09:42:27 +0700

    add: model relationships

--- app/Models/Packet.php
+++ app/Models/Packet.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Packet extends Model
 {
     use HasFactory;
+
+    public function treatments(): BelongsToMany
+    {
+        return $this->belongsToMany(Treatment::class);
+    }
 }
---
+++ app/Models/TreatmentCatagory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TreatmentCatagory extends Model
+{
+    use HasFactory;
+
+    public function treatments(): HasMany
+    {
+        return $this->hasMany(Treatment::class);
+    }
+}

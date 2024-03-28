a6ac52154b1d5317affb8c483eec7c83cce78803
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2024-03-28 14:14:00 +0700

    feat: auth login

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
 
+    public $timestamps = false;
     protected $fillable = [
         'name', 'email', 'password'
     ];

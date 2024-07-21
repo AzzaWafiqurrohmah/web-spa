82153fb532e162bab541142ab9f79ad409dff15c
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2024-07-21 18:10:35 +0700

    fix: icon on sidebar

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -33,10 +33,10 @@
       <li class="nav-item">
         <a href="/dashboard" class="nav-link d-flex align-items-center">
           <span class="sidebar-icon me-3">
-            <img src="/assets/img/brand/light.svg" height="20" width="20" alt="Volt Logo">
+            <img src="/assets/img/brand/icon.png" height="33" width="40" alt="Volt Logo">
           </span>
           <span class="mt-1 ms-1 sidebar-text">
-            web - spa
+            AromaTherapy Spa
           </span>
         </a>
       </li>

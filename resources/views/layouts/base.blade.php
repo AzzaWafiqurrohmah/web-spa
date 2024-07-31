bee28b6903c43610a375c0788671850cb668a053
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2024-07-31 10:54:50 +0700

    fix: icon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,12 +10,12 @@
 {{--              integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">--}}
 
         <!-- Favicons -->
-        <link rel="apple-touch-icon" href="../../assets/img/favicon/apple-touch-icon.png" sizes="180x180">
-        <link rel="icon" href="../../assets/img/favicon/favicon-32x32.png" sizes="32x32" type="image/png">
-        <link rel="icon" href="../../assets/img/favicon/favicon-16x16.png" sizes="16x16" type="image/png">
+        <link rel="apple-touch-icon" href=/assets/img/brand/icon.png" sizes="180x180">
+        <link rel="icon" href=/assets/img/brand/icon.png" sizes="32x32" type="image/png">
+        <link rel="icon" href=/assets/img/brand/icon.png" sizes="16x16" type="image/png">
 
         <link rel="mask-icon" href="../../assets/img/favicon/safari-pinned-tab.svg" color="#563d7c">
-        <link rel="icon" href="../../assets/img/favicon/favicon.ico">
+        <link rel="icon" href="/assets/img/brand/icon.png">
         <meta name="msapplication-config" content="../../assets/img/favicons/browserconfig.xml">
         <meta name="theme-color" content="#563d7c">
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
